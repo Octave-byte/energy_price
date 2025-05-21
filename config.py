71887420060492c27e08a470ac3f3104abd5ab73
octavionotpunk <83534113+Octave-byte@users.noreply.github.com>
2025-05-21 16:24:47 +0000

    change url db

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import streamlit as st
 SUPABASE_API_KEY = st.secrets["auth_token"]
-SUPABASE_URL = st.secrets["url"]
+SUPABASE_URL = 'https://qkonktvwcbfkehznugum.supabase.co'
+
 
 HEADERS = {
     "apikey": SUPABASE_API_KEY,
